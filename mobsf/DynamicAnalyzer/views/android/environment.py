cf842f35f682ad1b594e97debeef29d2c1316001
Damian Han <damiian28x@gmail.com>
2024-07-06 23:56:05 +0800

    Test.

--- mobsf/DynamicAnalyzer/views/android/environment.py
+++ mobsf/DynamicAnalyzer/views/android/environment.py
@@ -42,7 +42,6 @@ from mobsf.StaticAnalyzer.models import StaticAnalyzerAndroid
 logger = logging.getLogger(__name__)
 ANDROID_API_SUPPORTED = 34
 
-avd_name = get_avd_name()
 
 class Environment:
 
@@ -264,6 +263,7 @@ class Environment:
             logger.warning('mitmproxy root CA is not generated yet.')
             return
         if action == 'install':
+            avd_name = get_avd_name()
             logger.info('[%s] Installing MobSF RootCA', avd_name)
             self.adb_command(['push',mobsf_ca ,ca_file])
             self.adb_command(['chmod', '644', ca_file], True)
